ecbbcad933ef31884867f87616447a7a524d3ca6
CisinoJr <cisinojr@gmail.com>
2019-02-10 20:16:16 -0200

    Register Activity
    Implementation of Register activity

---
+++ app/src/main/java/br/com/cisinojr/wallet/controller/RegisterActivity.kt
@@ -0,0 +1,13 @@
+package br.com.cisinojr.wallet.controller
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import br.com.cisinojr.wallet.R
+
+class RegisterActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+    }
+}
