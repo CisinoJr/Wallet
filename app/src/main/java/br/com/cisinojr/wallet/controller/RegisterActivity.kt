fa0b9b9502fd266f0134d41ccc2a40a956c8c6ce
CisinoJr <cisinojr@gmail.com>
2019-02-10 22:24:17 -0200

    LoginActivity
    Add ClickListener to registration textview and method to go to RegisterActivity

--- app/src/main/java/br/com/cisinojr/wallet/controller/RegisterActivity.kt
+++ app/src/main/java/br/com/cisinojr/wallet/controller/RegisterActivity.kt
@@ -1,13 +1,43 @@
 package br.com.cisinojr.wallet.controller
 
-import android.support.v7.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.view.View
 import br.com.cisinojr.wallet.R
+import kotlinx.android.synthetic.main.activity_login.*
 
-class RegisterActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity(), View.OnClickListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_register)
+        setContentView(R.layout.activity_login)
+
+        // initializing the listeners
+        initListeners()
     }
+
+    /**
+     * This method is to initialize listeners
+     */
+    private fun initListeners() {
+        txtViewRegister!!.setOnClickListener(this)
+    }
+
+    /**
+     * This implemented method is to listen the click on view
+     *
+     * @param view
+     */
+    override fun onClick(view: View) {
+       when(view.id) {
+           // Go to registration view
+           R.id.txtViewRegister -> {
+               // Navigate to RegisterActivity
+               val intentRegister = Intent(applicationContext, RegisterActivity::class.java)
+               startActivity(intentRegister)
+           }
+       }
+    }
+
 }
