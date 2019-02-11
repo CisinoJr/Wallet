e75bff39b8b63a83fb4acf522cdaf2bf1f44d9ed
CisinoJr <cisinojr@gmail.com>
2019-02-10 22:25:26 -0200

    Register - Validation
    Add cpf validation to register action

--- app/src/main/java/br/com/cisinojr/wallet/controller/RegisterActivity.kt
+++ app/src/main/java/br/com/cisinojr/wallet/controller/RegisterActivity.kt
@@ -1,27 +1,35 @@
 package br.com.cisinojr.wallet.controller
 
-import android.content.Intent
-import android.os.Bundle
+import android.graphics.Color
 import android.support.v7.app.AppCompatActivity
-import android.view.View
+import android.os.Bundle
+import android.support.design.widget.Snackbar
+import android.support.v4.content.ContextCompat
 import br.com.cisinojr.wallet.R
-import kotlinx.android.synthetic.main.activity_login.*
+import android.view.View
+import android.widget.EditText
+import br.com.cisinojr.wallet.controller.util.CpfMask
+import br.com.cisinojr.wallet.controller.util.CpfValidator
+import kotlinx.android.synthetic.main.activity_register.*
 
-class LoginActivity : AppCompatActivity(), View.OnClickListener {
+class RegisterActivity : AppCompatActivity(), View.OnClickListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        setContentView(R.layout.activity_register)
 
         // initializing the listeners
         initListeners()
+
+        // initialize cpf mask
+        onCpfTextChange()
     }
 
     /**
      * This method is to initialize listeners
      */
     private fun initListeners() {
-        txtViewRegister!!.setOnClickListener(this)
+        btnConfirmRegister!!.setOnClickListener(this)
     }
 
     /**
@@ -30,14 +38,41 @@ class LoginActivity : AppCompatActivity(), View.OnClickListener {
      * @param view
      */
     override fun onClick(view: View) {
-       when(view.id) {
-           // Go to registration view
-           R.id.txtViewRegister -> {
-               // Navigate to RegisterActivity
-               val intentRegister = Intent(applicationContext, RegisterActivity::class.java)
-               startActivity(intentRegister)
-           }
-       }
+        when (view.id) {
+            R.id.btnConfirmRegister -> {
+                register()
+            }
+        }
+    }
+
+    /**
+     * Set CPF mask
+     */
+    private fun onCpfTextChange() {
+        editTextCpf.addTextChangedListener(CpfMask.mask("###.###.###-##", editTextCpf))
+    }
+
+    /**
+     * Register the user into database
+     */
+    private fun register() {
+        if (!CpfValidator.myValidateCPF(editTextCpf.text.toString())) {
+            showSnackFeedback("CPF inválido", false, editTextCpf)
+        }
+    }
+
+    /**
+     * Show validation error messages
+     */
+    fun showSnackFeedback(message: String, isValid: Boolean, view: View) {
+        val snackbar: Snackbar = Snackbar.make(view, message, Snackbar.LENGTH_SHORT)
+        val v: View = snackbar.view
+
+        if (!isValid) {
+            v.setBackgroundColor(ContextCompat.getColor(this, android.R.color.holo_red_dark))
+        }
+
+        snackbar.show()
     }
 
 }
