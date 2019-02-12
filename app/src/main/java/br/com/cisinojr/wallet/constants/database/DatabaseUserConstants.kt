afc51d25d9ea20ca0b951036e36e052b008c0dfe
CisinoJr <cisinojr@gmail.com>
2019-02-11 23:01:27 -0200

    Create database user constants

---
+++ app/src/main/java/br/com/cisinojr/wallet/constants/database/DatabaseUserConstants.kt
@@ -0,0 +1,15 @@
+package br.com.cisinojr.wallet.constants.database
+
+class DatabaseUserConstants {
+
+    val TABLE_NAME = "user"
+
+    companion object COLUMNS {
+        val ID = "id"
+        val FULLNAME = "fullname"
+        val EMAIL = "email"
+        val CPF = "cpf"
+        val PASSWORD = "password"
+    }
+
+}
